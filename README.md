8a8b478100696a1da6c54e6794f8d5d194538ae1
saubun <dameon0430@gmail.com>
2021-03-20 00:59:48 -0400

    Template created mostly

---
+++ README.md
@@ -0,0 +1,3 @@
+# My personal blog
+
+This is a repository containing the source code for my personal blog! :)
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
 window.onload = () => {
 	console.log('Hello World');
 };
+
+// Create TS to create new posts w/ objects
